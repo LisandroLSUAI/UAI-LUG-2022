bd8798c7fbb8de222e1604e5f3d46e92444ae83f
lisandro <lisandro.lopezserra@alumnos.uai.ar>
2022-09-20 21:24:52 -0300

    ADD blog model schema

--- blog/index.ts
+++ blog/index.ts
@@ -1,8 +1,15 @@
 import { Request,Response, Router } from "express";
+import blogModel from "../models/blog";
 const blogRouter = Router()
 
-blogRouter.get("/", (req: Request, res:Response) => {
-    res.send("Algo")
+blogRouter.get("/:title", async (req: Request, res:Response) => {
+    try {
+        const title = req.params.title
+        const blog = await blogModel.findOne({ title }).exec();
+        res.send(blog)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 export default blogRouter
---
+++ models/blog/index.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose';
+
+const blogSchema = new Schema({
+  title:  String, // String is shorthand for {type: String}
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
+
+const blogModel = model("blog", blogSchema)
+
+export default blogModel
